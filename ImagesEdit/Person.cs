c4b1065b28cf8a9021fa7544f45d76a89cbed9cf
Andrii Shema <andshema@gmail.com>
2021-03-12 15:51:23 +0200

    Добавьте файлы проекта.

---
+++ ImagesEdit/Person.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImagesEdit
+{
+    class Person
+    {
+        private string name;
+        private int speed;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Speed
+        {
+            get { return speed; }
+            set
+            {
+                if (value < 0)
+                { Console.WriteLine("value must be greater than zero "); }
+                else
+                { speed = value; }
+            }
+        }
+
+        public Person(string name, int speed)
+        {
+            this.name = name;
+            this.speed = speed;
+        }
+
+        // how many photos each worker will edit 
+        public static void Each_Make(double allImages, int restImage, int speed1, int speed2, int speed3)
+        {
+            int makeImages1 = 0;
+            int makeImages2 = 0;
+            int makeImages3 = 0;
+
+            // check the productivity of each employee for the total time 
+            for (int i = 0; i <= allImages; i++)
+            {
+                if ((i % speed1 == 0) & (i != 0) & restImage != 0)
+                {
+                    makeImages1 += 1;
+                    restImage -= 1;
+                }
+                if ((i % speed2 == 0) & (i != 0) & restImage != 0)
+                {
+                    makeImages2 += 1;
+                    restImage -= 1;
+                }
+
+                if ((i % speed3 == 0) & (i != 0) & restImage != 0)
+                {
+                    makeImages3 += 1;
+                    restImage -= 1;
+                }
+            }
+
+            Console.WriteLine("Person1 edit: {0} images ", makeImages1);
+            Console.WriteLine("Person2 edit: {0} images ", makeImages2);
+            Console.WriteLine("Person3 edit: {0} images ", makeImages3);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ImagesEdit/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImagesEdit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person("person1", 2);
+            Person person2 = new Person("person2", 3);
+            Person person3 = new Person("person3", 4);
+
+            Console.WriteLine("Enter the number of pictures to edit: ");
+            int allImages = Int32.Parse(Console.ReadLine());
+
+            //the total time for which the work will be done.  
+            double totalTime =Math.Ceiling((allImages / ((double)1 / person1.Speed + (double)1 / person2.Speed + (double)1 / person3.Speed)));
+            Console.WriteLine("The total working time is : {0} minutes ", totalTime);
+
+            int restImage = allImages;
+            int speed1 = person1.Speed;
+            int speed2 = person2.Speed;
+            int speed3 = person3.Speed;
+
+            // show personal work 
+            Person.Each_Make(totalTime, restImage, speed1, speed2, speed3);
+
+        }
+
+        
+    }  
+}
